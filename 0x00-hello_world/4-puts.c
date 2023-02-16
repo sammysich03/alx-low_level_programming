6946ca2b83c0c9442aafe0f86f1011ecba5509ce
oluku samson <sammysich03@gmail.com>
2023-02-16 14:14:02 -0800

    task 6

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
  * maain - entring point
- * return always 0 (success)
+ * return always 0(success)
 **/
 
 int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-			return(0);
+{	puts("\"Programming is like building a multilingual puzzle");
+			return (0);
 }
