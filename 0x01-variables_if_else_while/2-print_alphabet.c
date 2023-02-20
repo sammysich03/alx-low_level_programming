86352ba8ed533cbb3aec60390d9afc737fc5b26a
oluku samson <sammysich03@gmail.com>
2023-02-20 00:29:02 -0800

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Use `putchar` function to print the alphabet in lowercase.
+ * Return: 0
+ */
+
+int main(void)
+{
+	char a;
+
+	a = 'b'
+
+	while (a <= 'z')
+	{
+		putchar("%c", a);
+	}
+		putchar("\n");
+	return (0);
+}
