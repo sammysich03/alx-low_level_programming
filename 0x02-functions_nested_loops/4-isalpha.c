cb111748005cf9769433468f0916fff6848697e8
oluku samson <sammysich03@gmail.com>
2023-02-22 06:02:44 -0800

    task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,8 +2,8 @@
 /**
  * _isalpha - Checks for aphabetical letters
  * @c:  a character check on
- * Return: return 1 and 0 depending on the condition  
- */ 
+ * Return: return 1 and 0 depending on the condition
+ */
 int _isalpha(int c)
 {
 	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
