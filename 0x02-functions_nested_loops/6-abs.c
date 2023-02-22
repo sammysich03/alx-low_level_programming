01d8a0b73651ba32ef2a6711374bc226edee2954
oluku samson <sammysich03@gmail.com>
2023-02-22 08:04:07 -0800

    task 5

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,12 +1,16 @@
 #include "main.h"
 /**
- * _abs -> is the entring point
- * *@n: is a parameter
- *return: absolute value of a number
+ * _abs - returns absolute value of an int
+ * @n: integer from which to get an absolute value
+ * Return: Void.
  */
+
 int _abs(int n)
 {
+
 	if (n < 0)
-		n = -n;
+	{
+		return (n * -1);
+	}
 	return (n);
 }
