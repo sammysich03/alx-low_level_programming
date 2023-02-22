239b134f5084df88ca929407420cba27e2e2d746
oluku samson <sammysich03@gmail.com>
2023-02-22 06:54:07 -0800

    task 67

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _abs -> is the entring point
+ * *@n: is a parameter
+ *return: absolute value of a number
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		n = -n;
+	return (n);
+}
