f8daf2cac22a9a06b6ee471aa95f85828e77c93e
oluku samson <sammysich03@gmail.com>
2023-02-22 04:58:42 -0800

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,11 @@
 #include "main.h"
 /**
  *print_alphabet_x10 - entring point
- *it print a to z 10x
  *return: always success
  */
 void print_alphabet_x10(void)
 {
 	int j = 0;
-	int i;
 
 	while (j < 10)
 	{
