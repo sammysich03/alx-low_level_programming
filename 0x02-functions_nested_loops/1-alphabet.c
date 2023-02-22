11492d1aebf9cf56412221042ccd84e6019f85de
oluku samson <sammysich03@gmail.com>
2023-02-22 02:45:15 -0800

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *main - entring
+ *return: always success
+ */
+
+void print_alphabet(void);
+{
+	int b = 'a';
+
+	for (b > 'z')
+		_putchar("%d" b);
+			b++;
+
+	return (0);
+}
